8c53a2b50582ec2aaf048aa5cb7bdacbdcdb6878
coderzzz <824530369@qq.com>
2022-02-15 17:47:04 +0800

    完成用户验证前端代码实现

---
+++ blog-community-mgr-system-fe/src/views/Auth/index.js
@@ -0,0 +1,13 @@
+import {defineComponent} from 'vue';
+import {UserOutlined,LockOutlined,GiftOutlined} from '@ant-design/icons-vue';
+
+export default defineComponent({
+  setup(){
+
+  },
+  components:{
+    UserOutlined,
+    LockOutlined,
+    GiftOutlined
+  }
+})
---
+++ blog-community-mgr-system-fe/src/views/Auth/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="auth">
+    <div class="bg"></div>
+    <div class="titie-info">
+      <div class="logo">
+        <img src="../../assets/img/logo.png" alt="">
+      </div>
+      <h2 class="title">博客信息后台管理系统</h2>
+    </div>
+
+    <div class="form">
+      <a-tabs>
+        <a-tab-pane key="1" tab="登录">
+          <div class="item">
+            <a-input size="large" placeholder="账号">
+              <template #prefix>
+                <UserOutlined />
+              </template>
+            </a-input>
+          </div>
+          <div class="item">
+            <a-input size="large" placeholder="密码">
+              <template #prefix>
+                <LockOutlined />
+              </template>
+            </a-input>
+          </div>
+
+          <div class="item forget">
+            <a href="">忘记密码</a>
+          </div>
+
+          <div class="item">
+            <a-button size="large" type="primary">登录</a-button>
+          </div>
+        </a-tab-pane>
+        <a-tab-pane key="2" tab="注册">
+          <div class="item">
+            <a-input size="large" placeholder="账号">
+              <template #prefix>
+                <UserOutlined />
+              </template>
+            </a-input>
+          </div>
+          <div class="item">
+            <a-input size="large" placeholder="密码">
+              <template #prefix>
+                <LockOutlined />
+              </template>
+            </a-input>
+          </div>
+          <div class="item">
+            <a-input size="large" placeholder="邀请码">
+              <template #prefix>
+                <GiftOutlined />
+              </template>
+            </a-input>
+          </div>
+
+          <div class="item">
+            <a-button size="large" type="primary">注册</a-button>
+          </div>
+        </a-tab-pane>
+      </a-tabs>
+    </div>
+  </div>
+</template>
+
+<script src="./index.js"></script>
+
+<style lang="scss" scpoed>
+ @import './index.scss'
+</style>
